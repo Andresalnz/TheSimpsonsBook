f54de1afecd2eaf7340be9889872c3dc1033f3d3
Andresalnz <andresaleunz@gmail.com>
2025-11-24 19:11:42 +0100

    Edit Model

--- TheSimpsonsBook/Model/SwifData/FavouriteModel.swift
+++ TheSimpsonsBook/Model/SwifData/FavouriteModel.swift
@@ -8,20 +8,35 @@
 import Foundation
 import SwiftData
 
-
-enum FavouriteType: String, Codable, CaseIterable, Sendable {
+enum FavouriteType: String, Codable, CaseIterable, Identifiable {
     case character
     case episode
     case location
+    
+    var id: String { rawValue }
+    
+    var title: String {
+        switch self {
+            case .character: return "character"
+            case .episode: return "Episodes"
+            case .location: return "Locations"
+        }
+    }
 }
 
 @Model
 final class FavouriteModel {
     
-    var type: FavouriteType
+    //var type: FavouriteType
+    var typeRaw: String = FavouriteType.character.rawValue
+    @Transient // Le decimos a SwiftData que NO guarde esto, ya guardamos typeRaw
+        var type: FavouriteType {
+            get { FavouriteType(rawValue: typeRaw) ?? .character }
+            set { typeRaw = newValue.rawValue }
+        }
+    
     var remoteId: Int
 
-    
     var title: String?
     var subtitle: String?
     var imageURL: URL?
@@ -29,7 +44,7 @@ final class FavouriteModel {
     var createdAt: Date
 
     init(type: FavouriteType, remoteId: Int, title: String? = nil, subtitle: String? = nil, imageURL: URL? = nil, createdAt: Date) {
-        self.type = type
+        self.typeRaw = type.rawValue
         self.remoteId = remoteId
         self.title = title
         self.subtitle = subtitle
@@ -40,10 +55,9 @@ final class FavouriteModel {
 
 extension FavouriteModel {
    @MainActor static let sampleData = [
-        FavouriteModel(type: .character, remoteId: 1, createdAt: .now),
-        FavouriteModel(type: .character, remoteId: 1, createdAt: .now),
-        FavouriteModel(type: .character, remoteId: 1, createdAt: .now),
-        FavouriteModel(type: .character, remoteId: 1, createdAt: .now)
-        
+        FavouriteModel(type: .character, remoteId: 1,title: "character", subtitle: "subtitle", imageURL: URL(string: "https://cdn.thesimpsonsapi.com/500/character/1.webp"), createdAt: .now),
+        FavouriteModel(type: .character, remoteId: 2,title: "character", subtitle: "subtitle", imageURL: URL(string: "https://cdn.thesimpsonsapi.com/500/character/1.webp"), createdAt: .now),
+        FavouriteModel(type: .episode, remoteId: 3,title: "episode", subtitle: "subtitle", imageURL: URL(string: "https://cdn.thesimpsonsapi.com/500/character/1.webp"), createdAt: .now),
+        FavouriteModel(type: .location, remoteId: 4,title: "location", subtitle: "subtitle", imageURL: URL(string: "https://cdn.thesimpsonsapi.com/500/character/1.webp"), createdAt: .now)
     ]
 }
